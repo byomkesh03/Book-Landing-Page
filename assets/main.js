9e6aabd81b3cb0ce37a63ebaf1bce94270669b41
Byomkesh Mahato <byomkeshmahato03@gmail.com>
2023-06-15 00:31:51 +0530

    added close ico, nav style

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,7 @@
 function toggleMenu(displayState) {
      const nav = document.getElementById('header_navigation');
-        
+     const closeIco = document.getElementById('close-ico');
+    nav.style.display = displayState;
+    closeIco.style.display = displayState;
+   
 }
